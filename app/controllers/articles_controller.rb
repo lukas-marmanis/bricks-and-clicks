46f43396ada07d87d918210cd9f237ca84f86eee
Lukas Marmanis <lukas@marmanis.com>
2023-08-03 21:07:26 -0400

    Uploading the code used to migrate articles into the db. Also added comments throughout the project for clarity's sake.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,25 @@
 class ArticlesController < ApplicationController
   # GET /articles
   def index
+    # Fetch articles from the database, ordered by published_at in descending order
     articles = Article.order(published_at: :desc)
+
+    # Render the articles as JSON response
     render json: articles
   end
 
   # POST /articles
   def create
+    # Extract article parameters from the request
     article_params = params.permit(:title, :content, :author, :category, :published_at)
 
+    # Check if article_params are empty
     if article_params.blank?
       render json: { message: 'Article not found' }, status: :not_found
       return
     end
 
+    # Attempt to create a new article with the provided parameters
     begin
       article = Article.create!(
         title: article_params[:title],
@@ -22,37 +28,49 @@ class ArticlesController < ApplicationController
         category: article_params[:category],
         published_at: article_params[:published_at]
       )
+
+      # If the article is successfully created, render it as JSON response with status code 201 (created)
       render json: article, status: :created
     rescue ActiveRecord::RecordInvalid => e
+      # If the article creation fails due to validation errors, render the error message with status code 422 (unprocessable_entity)
       render json: { error: e.message }, status: :unprocessable_entity
     end
   end
 
   # GET /articles/:id
   def show
+    # Find the article by the provided id
     article = Article.find_by(id: params[:id])
+
+    # Check if the article is found
     if article
+      # Render the article as JSON response
       render json: article
     else
+      # If the article is not found, render a JSON response with status code 404 (not_found)
       render json: { message: 'Article not found' }, status: :not_found
     end
   end
 
   # Handling DELETE, PUT, and PATCH requests for /articles/:id
   def destroy
+    # Respond with status code 405 (method_not_allowed) for delete requests
     head :method_not_allowed
   end
 
   def update
+    # Respond with status code 405 (method_not_allowed) for update requests
     head :method_not_allowed
   end
 
   def patch
+    # Respond with status code 405 (method_not_allowed) for patch requests
     head :method_not_allowed
   end
 
   private
 
+  # Method to permit and extract article parameters from the request
   def article_params
     params.permit(:title, :content, :author, :category, :published_at)
   end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,16 @@
 class Article < ApplicationRecord
+  # Model validations
   validates :title, presence: true, length: { maximum: 40 }
   validates :content, :author, :category, :published_at, presence: true
 
+  # Custom as_json method to format the JSON response
   def as_json(options = {})
+    # Call the original as_json method of ApplicationRecord to generate the initial JSON hash
     super(except: [:created_at, :updated_at]).tap do |hash|
+      # Format the published_at attribute to display only the date in 'YYYY-MM-DD' format
       hash['published_at'] = published_at.strftime('%Y-%m-%d')
     end
   end
 end
+
 
