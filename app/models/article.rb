1dd0ab93b01c794e58f77c3824897905e1b68200
Lukas Marmanis <lukas@marmanis.com>
2023-08-03 20:18:37 -0400

    Modified the article class' as_json method to address formatting in tests. All tests now pass!

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,11 @@
 class Article < ApplicationRecord
-
   validates :title, presence: true, length: { maximum: 40 }
   validates :content, :author, :category, :published_at, presence: true
 
+  def as_json(options = {})
+    super(except: [:created_at, :updated_at]).tap do |hash|
+      hash['published_at'] = published_at.strftime('%Y-%m-%d')
+    end
+  end
 end
+
